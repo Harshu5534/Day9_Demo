146d9c153814101f46ebf0611500c554f12395ff
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-03-29 23:02:36 +0530

    Part Time added

--- calculateEmpWage.sh
+++ calculateEmpWage.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -x
 
 PRESENT=1;
+PART_TIME=2;
 WAGE_PER_HOUR=20;
 
-isPresent=$((RANDOM%2));
+isPresent=$((RANDOM%3));
 
 if [ $isPresent -eq $PRESENT ]
 then
 	WorkingHour=8;
+
+elif [ $isPresent -eq $PART_TIME ]
+then
+	WorkingHour=4;
 else
 	WorkingHour=0;
 fi
